3a2996869f03a596e58cad6b38764d502c6f7b1e
Venkatesh Srinivasan <venk@google.com>
2026-03-06 22:10:57 +0000

    Disable -Wincompatible-pointer-types
    There are only a few occurrences, but the fixes are somewhat
    non-trivial. Filing a bug and suppressing the warning to unblock
    the r596125 update.
    
    Bug: 472333761
    Test: presubmit
    Change-Id: I178f80ad7dcd33cbbf2f188794c6ce497a94dd42
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- cc/config/global.go
+++ cc/config/global.go
@@ -332,6 +332,8 @@ var (
 		"-Wno-character-conversion", // http://b/452740154
 		// TODO: Disable this warning in external projects after switching clang.
 		"-Wno-error=uninitialized-const-pointer", // http://b/458489157
+		// New warnings to be fixed after clang-r596125
+		"-Wno-incompatible-pointer-types", // http://b/490481169
 
 		// Allow using VLA CXX extension.
 		"-Wno-vla-cxx-extension",
