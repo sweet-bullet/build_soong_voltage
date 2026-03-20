f16751c8597d8cd4abf791eb9d0d791c5f06723b
Justin Yun <justinyun@google.com>
2026-03-20 23:56:47 +0900

    Run strict artifact path requirement test only for Soong-only
    When PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS is set to `strict`,
    PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST must not have unused
    list. However, modules defined in Android.mk is not visible to Soong
    and missing for this test. The error message already explains this,
    but Kati-enabled build still may have Android.mk defined modules.
    In that case, the test runs from artifact_path_requirements.mk.
    So run this test only for Soong-only.
    
    Bug: 481293620
    Test: build with offending Android.mk module.
    Change-Id: Ibf58a4d259dfe5af82d7b2b7ba5e1f91f10890a2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- fsgen/artifact_path_requirements.go
+++ fsgen/artifact_path_requirements.go
@@ -219,7 +219,8 @@ func (s *artifactPathRequirementsVerifierSingleton) GenerateBuildActions(ctx and
 	}
 	for _, makefile := range android.SortedKeys(internalUnusedAllowedList) {
 		sort.Strings(internalUnusedAllowedList[makefile])
-		if !partitionVars.ArtifactPathRequirementsIsRelaxedOfMakefile[makefile] {
+		if !partitionVars.ArtifactPathRequirementsIsRelaxedOfMakefile[makefile] && !ctx.Config().KatiEnabled() {
+			// Skip this check for KatiEnabled build that has Android.mk
 			errMsg := fmt.Sprintf("%s includes redundant allowed entries in its artifact path requirement.\n", makefile)
 			errMsg += "If the modules are defined in Android.mk, They might be missing from the verification. Define the modules in Android.bp instead.\n"
 			errMsg += "Otherwise, remove the redundant allowed entries.\n"
@@ -245,7 +246,8 @@ func (s *artifactPathRequirementsVerifierSingleton) GenerateBuildActions(ctx and
 			errMsg += "Please define a vintf_compatibility_matrix module in Android.bp for each FCM file with 'system_ext_specific: true' or the other partition that implements the feature. Then add each of these vintf_compatibility_matrix modules to PRODUCT_PACKAGES."
 			printListAndError(ctx, externalOffendingFcms[makefile], errMsg)
 		}
-		if enforcement != "relaxed" {
+		if enforcement != "relaxed" && !ctx.Config().KatiEnabled() {
+			// Skip this check for KatiEnabled build that has Android.mk
 			for _, makefile := range android.SortedKeys(externalUnusedAllowedList) {
 				sort.Strings(externalUnusedAllowedList[makefile])
 				errMsg := fmt.Sprintf("Device makefile includes redundant artifact path requirement allowed list entries in %s.\n", makefile)
